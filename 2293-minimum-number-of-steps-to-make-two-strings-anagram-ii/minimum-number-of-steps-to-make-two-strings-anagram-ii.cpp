b90bc8084cd1484241d7e9a546f57f04ac46a86e
taarumgarud <gautamrudrav@gmail.com>
2026-07-07 22:12:09 +0530

    Time: 120 ms (8.12%) | Memory: 32 MB (17.68%) - LeetSync

--- 2293-minimum-number-of-steps-to-make-two-strings-anagram-ii/minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
+++ 2293-minimum-number-of-steps-to-make-two-strings-anagram-ii/minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
-        unordered_map<char,int> mp;
+        map<char,int> mp;
         for(int i = 0; i<s.size(); i++){
             mp[s[i]]++;
         }
