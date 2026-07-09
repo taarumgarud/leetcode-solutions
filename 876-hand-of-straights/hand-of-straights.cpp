6ff91099fe2910cf28695f8fa9f442c2c6307b7b
taarumgarud <gautamrudrav@gmail.com>
2026-07-09 22:20:32 +0530

    Time: 52 ms (10.08%) | Memory: 35.4 MB (5.62%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -6,32 +6,27 @@ public:
         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
         for(auto i : mp) pq.push(i);
         vector<pair<int,int>> v;
-        int cnt = groupSize;
-        // bool ok = true;
+        bool ok = true;
         while(!pq.empty()){
             int num = pq.top().first;
             int freq = pq.top().second;
             pq.pop();
             
-            if (v.size() >= 1 && v.back().first + 1 != num)
-            {
-                return false;
-            }
-            else 
-            {
-                v.push_back({num , freq-1}) ;
+            if(v.empty() || (v.size() > 0 && num == v.back().first+1)) v.push_back({num,freq-1});
+            else{
+                ok = false;
+                break;
             }
 
-            if(v.size() == cnt){
-                for(auto i : v)
-                {
-                    if (i.second >= 1) pq.push(i) ;
+            if(v.size() == groupSize){
+                for(auto i : v){
+                    if(i.second > 0) pq.push(i);
                 }
                 v.clear();
             }
         }
-        if (v.size() == 0) return true ;
-        else return false ;
-        // return true ;
+
+        if (v.size() != 0) return false ;
+        return ok;
     }
 };
